fd646d9d9208d66abec5b18cfab79c40c76f9e9d
DanishAli01 <danishali2158@gmail.com>
2019-03-14 01:35:02 +0000

    Dialogue Box o/n profile in GrindView & Image Reader Class addition

--- app/src/main/java/com/example/danishali/assignment03/myapplication/ImageReader.java
+++ app/src/main/java/com/example/danishali/assignment03/myapplication/ImageReader.java
@@ -8,17 +8,18 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.TextView;
+
 import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.Detector;
 import com.google.android.gms.vision.text.TextBlock;
 import com.google.android.gms.vision.text.TextRecognizer;
+
 import java.io.IOException;
 
 public class ImageReader extends AppCompatActivity {
